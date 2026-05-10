57e2094b00f2d10eb36f5531c5fa9ce42c16db5f
Gideon McKinlay <gmckinlay04@gmail.com>
2026-05-10 11:54:52 +1000

    it's green now

--- barGenerate.py
+++ barGenerate.py
@@ -1,20 +1,32 @@
 from PIL import Image
 
-h = 1080 + 255
-w = 25
+# h = 1080 + 255
+# w = 25
+# img = Image.new("RGBA", (w, h))
+
+# for y in range(h):
+#     print(f"Generating barfadeRed.png: {y}/{h}", end="\r")
+#     for x in range(w):
+#         img.putpixel((x, h-y-1), (255, 64, 96, min(y,255)))
+
+# img.save("barfadeRed.png")
+
+# for y in range(h):
+#     print(f"Generating barfadeGreen.png: {y}/{h}", end="\r")
+#     for x in range(w):
+#         img.putpixel((x, h-y-1), (98, 180, 85, min(y,255)))
+
+# img.save("barfadeGreen.png")
+
+
+h = 50
+w = 1185
 img = Image.new("RGBA", (w, h))
 
-
 for y in range(h):
-    print(f"Generating barfadeRed.png: {y}/{h}", end="\r")
+    print(f"Generating choice_hover_background.png: {y}/{h}", end="\r")
     for x in range(w):
-        img.putpixel((x, h-y-1), (255, 64, 96, min(y,255)))
+        a = min(x, w-x, 255)
+        img.putpixel((x, h-y-1), (130, 189, 127, a))
 
-img.save("barfadeRed.png")
-
-for y in range(h):
-    print(f"Generating barfadeGreen.png: {y}/{h}", end="\r")
-    for x in range(w):
-        img.putpixel((x, h-y-1), (98, 180, 85, min(y,255)))
-
-img.save("barfadeGreen.png")
+img.save("choice_hover_background.png")
